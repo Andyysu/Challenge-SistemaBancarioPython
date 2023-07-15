dfaaedae2d8db1f9f9302b0cbdb7115390591b6d
Andyysu <als90and@gmail.com>
2023-07-15 17:33:29 -0300

    git commit -a

--- challenge001.py
+++ challenge001.py
@@ -1,9 +1,9 @@
 menu = """
 
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[0] Sair
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
 
 => """
 
@@ -17,16 +17,41 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == 1:
-        print("Depósito")
-    
-    elif opcao == 2:
-        print("Saque")
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
 
-    elif opcao == 3:
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+    
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        #excedeu_saldo = valor > saldo
+
+        #excedeu_limite = valor > limite
+
+        #excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if valor > saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif valor > limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Operação falhou! Número máximo de saques excedido.")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
         print("Extrato")
     
-    elif opcao == 0:
+    elif opcao == "q":
         break
 
     else:
